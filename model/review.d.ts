5107c06d12f3c70b47871d13577383a8af36219a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-10-20 23:28:16 +0200

    Contract for review and collection of reviews

---
+++ model/review.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Contract for hotel review
+ * @interface Review
+ */
+export interface Review {
+  title: string;
+  text: string;
+  image: string;
+}
---
+++ model/reviews.d.ts
@@ -0,0 +1,9 @@
+import { Review } from './review';
+
+/**
+ * Contract for list of reviews
+ * @export
+ * @interface Reviews
+ * @extends {Array<Review>}
+ */
+export interface Reviews extends Array<Review> {}
