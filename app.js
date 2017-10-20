33bf344f602ff6703071bcb6d286b42359f2cba1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-10-20 22:59:43 +0200

    :fire: transpiled output

--- app.js
+++
@@ -1,5 +0,0 @@
-require('dotenv-extended').load();
-const builder = require('botbuilder');
-const restify = require('restify');
-const Store = require('store');
-const spellService = require('./spell-service');
